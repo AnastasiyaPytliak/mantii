5d3a2e8e446d7420d32b28beb244f7fc3426a230
Anastasia <nastya.pytliak@gmail.com>
2023-06-12 23:58:54 +0300

    add js, css for 3 and 4 steps

--- js/script.js
+++ js/script.js
@@ -41,6 +41,11 @@ const step2 = document.querySelector('.step-2')
 const step1AtiveBakalavr = document.querySelector('.step-1__btn .btn.active')
 const magistrImg = document.querySelector('.magistr-img img')
 const bakalavrImg = document.querySelector('.bakalavr-img img')
+const stepImg = document.querySelector('.step-3__img img')
+const stepHoodShow = document.querySelector('.step-3__hood')
+const stepBrushShow = document.querySelector('.brush__block select')
+const buySizeCount = document.querySelector('.buy-size__count')
+const rentSizeCount = document.querySelector('.rent-size__count')
 
 nextBtn.forEach(button=>{
   button.addEventListener('click', () => {
@@ -65,7 +70,6 @@ function changeStep (btn) {
     index--
   }
   steps[index].classList.add('active-step')
-  console.log(index);
 }
 
 function handleClickToggleActive (e) {
@@ -75,87 +79,6 @@ function handleClickToggleActive (e) {
     })
       e.target.classList.add('active')
     }
-}
-
-function handleClickActiveColor (e) {
-  if (e.target.classList.contains('color')) {
-    document.querySelectorAll('input.color').forEach(i => {
-      i.classList.remove('active')
-    })
-    switch(e.target.value) {
-      case 'cиний':
-        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
-        break;
-      case 'красный':
-        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-red.jpg'
-        break;
-      case 'золотой':
-        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-gold.jpg'
-        break;
-      case 'серебрянный':
-        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-silver.jpg'
-        break;
-      case 'фиолетовый':
-        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-purple.jpg'
-        break;
-      case 'голубой':
-        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-light-blue.jpg'
-        break;
-      default:
-        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
-    }
-  } else if (e.target.classList.contains('color-mg')) {
-      document.querySelectorAll('input.color-mg').forEach(i => {
-        i.classList.remove('active')
-      })
-      switch(e.target.value) {
-        case 'cиний':
-          magistrImg.src = './image/magistr/magistr-blue.jpg'
-          break;
-        case 'красный':
-          magistrImg.src = './image/magistr/magistr-red.jpg'
-          break;
-        case 'золотой':
-          magistrImg.src = './image/magistr/magistr-gold.jpg'
-          break;
-        case 'серебрянный':
-          magistrImg.src = './image/magistr/magistr-silver.jpg'
-          break;
-        case 'фиолетовый':
-          magistrImg.src = './image/magistr/magistr-purple.jpg'
-          break;
-        case 'голубой':
-          magistrImg.src = './image/magistr/magistr-light-blue.jpg'
-          break;
-        default:
-          magistrImg.src = './image/magistr/magistr-blue.jpg'
-      }
-    } else if (e.target.classList.contains('color-tie')) {
-      document.querySelectorAll('input.color-tie').forEach(i => {
-        i.classList.remove('active')
-      })
-      switch(e.target.value) {
-        case 'cиний':
-          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
-          break;
-        case 'красный':
-          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-red.png'
-          break;
-        case 'золотой':
-          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-gold.png'
-          break;
-        case 'белый':
-          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-silver.png'
-          break;
-        case 'зеленый':
-          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-green.jpg'
-          break;
-        default:
-          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
-      }
-    }
-  e.target.classList.add('active')
-
 }
 
 function handleClickActive (e) {
@@ -171,12 +94,15 @@ function handleClickActive (e) {
     switch(activeValue) {
       case 'просто':
         priceBakalavr.textContent = '1450'
+        stepHoodShow.setAttribute('style', 'display:none')
         break;
       case 'с капюшоном':
         priceBakalavr.textContent = '1750'
+        stepHoodShow.setAttribute('style', 'display:block')
         break;
       case 'с галстуком':
         priceBakalavr.textContent = '1650'
+        stepHoodShow.setAttribute('style', 'display:none')
       break;
 
     }
@@ -189,6 +115,7 @@ function handleClickActive (e) {
       e.target.classList.add('active')
 
       priceBakalavr.textContent = '1750'
+      stepHoodShow.setAttribute('style', 'display:block')
 
       stepBakalavr.querySelectorAll('.active').forEach(i => {
         i.classList.remove('active')
@@ -205,17 +132,121 @@ function handleClickShowColor (e) {
       pickColorElement.setAttribute('style', 'display:block')
       pickColorTieElement.setAttribute('style', 'display:none')
       bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
+      stepImg.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
       document.querySelector(".bakalavr .step-1__tabs-color .color").classList.add('active')
     } else if (e.target.value === 'с галстуком' && e.target.classList.contains("active")) {
       pickColorElement.setAttribute('style', 'display:none')
       pickColorTieElement.setAttribute('style', 'display:block')
       bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
+      stepImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
       document.querySelector(".bakalavr-tie .step-1__tabs-color .color-tie").classList.add('active')
     } else {
       bakalavrImg.src = './image/bakalavr.png'
+      stepImg.src = './image/bakalavr.png'
       pickColorElement.setAttribute('style', 'display:none')
       pickColorTieElement.setAttribute('style', 'display:none')
     }
+}
+
+function handleClickActiveColor (e) {
+  if (e.target.classList.contains('color')) {
+    document.querySelectorAll('input.color').forEach(i => {
+      i.classList.remove('active')
+    })
+    switch(e.target.value) {
+      case 'cиний':
+        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
+        stepImg.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
+        break;
+      case 'красный':
+        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-red.jpg'
+        stepImg.src = './image/bakalavr-hood/bakalavr-hood-red.jpg'
+        console.log('dfd');
+        break;
+      case 'золотой':
+        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-gold.jpg'
+        stepImg.src = './image/bakalavr-hood/bakalavr-hood-gold.jpg'
+        break;
+      case 'серебрянный':
+        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-silver.jpg'
+        stepImg.src = './image/bakalavr-hood/bakalavr-hood-silver.jpg'
+        break;
+      case 'фиолетовый':
+        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-purple.jpg'
+        stepImg.src.src = './image/bakalavr-hood/bakalavr-hood-purple.jpg'
+        break;
+      case 'голубой':
+        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-light-blue.jpg'
+        stepImg.src.src = './image/bakalavr-hood/bakalavr-hood-light-blue.jpg'
+        break;
+      default:
+        bakalavrImg.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
+        stepImg.src.src = './image/bakalavr-hood/bakalavr-hood-bue.png'
+    }
+  } else if (e.target.classList.contains('color-mg')) {
+      document.querySelectorAll('input.color-mg').forEach(i => {
+        i.classList.remove('active')
+      })
+      switch(e.target.value) {
+        case 'cиний':
+          magistrImg.src = './image/magistr/magistr-blue.jpg'
+          stepImg.src = './image/magistr/magistr-blue.jpg'
+          break;
+        case 'красный':
+          magistrImg.src = './image/magistr/magistr-red.jpg'
+          stepImg.src = './image/magistr/magistr-red.jpg'
+          break;
+        case 'золотой':
+          magistrImg.src = './image/magistr/magistr-gold.jpg'
+          stepImg.src = './image/magistr/magistr-gold.jpg'
+          break;
+        case 'серебрянный':
+          magistrImg.src = './image/magistr/magistr-silver.jpg'
+          stepImg.src = './image/magistr/magistr-silver.jpg'
+          break;
+        case 'фиолетовый':
+          magistrImg.src = './image/magistr/magistr-purple.jpg'
+          stepImg.src = './image/magistr/magistr-purple.jpg'
+          break;
+        case 'голубой':
+          magistrImg.src = './image/magistr/magistr-light-blue.jpg'
+          stepImg.src = './image/magistr/magistr-light-blue.jpg'
+          break;
+        default:
+          magistrImg.src = './image/magistr/magistr-blue.jpg'
+          stepImg.src = './image/magistr/magistr-blue.jpg'
+      }
+    } else if (e.target.classList.contains('color-tie')) {
+      document.querySelectorAll('input.color-tie').forEach(i => {
+        i.classList.remove('active')
+      })
+      switch(e.target.value) {
+        case 'cиний':
+          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
+          stepImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
+          break;
+        case 'красный':
+          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-red.png'
+          stepImg.src = './image/bakalavr-tie/bakalavr-tie-red.png'
+          break;
+        case 'золотой':
+          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-gold.png'
+          stepImg.src = './image/bakalavr-tie/bakalavr-tie-gold.png'
+          break;
+        case 'белый':
+          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-silver.png'
+          stepImg.src = './image/bakalavr-tie/bakalavr-tie-silver.png'
+          break;
+        case 'зеленый':
+          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-green.jpg'
+          stepImg.src = './image/bakalavr-tie/bakalavr-tie-green.jpg'
+          break;
+        default:
+          bakalavrImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
+          stepImg.src = './image/bakalavr-tie/bakalavr-tie-blue.png'
+      }
+    }
+  e.target.classList.add('active')
 }
 
 function handleSubmitForm (e) {
@@ -234,21 +265,15 @@ function handleSubmitForm (e) {
 
 function handleCountBuy (e) {
   if (e.target.classList.contains("plus")) {
-    // if (0 >= e.target.parentElement.querySelector("input").value <= 9) {
-    //   ++e.target.parentElement.querySelector("input").value;
-    //   console.log(e.target.parentElement.querySelector("input").value);
-    //   e.target.parentElement.querySelector("input").value = `0${e.target.parentElement.querySelector("input").value}`
-    // } else if (e.target.parentElement.querySelector("input").value >= 10) {
-    //   ++e.target.parentElement.querySelector("input").value;
-    //   console.log(e.target.parentElement.querySelector("input").value);
-    // }
     ++e.target.parentElement.querySelector("input").value;
     inputSumValue.value = e.target.parentElement.querySelector("input").value
+    buySizeCount.textContent = e.target.parentElement.querySelector("input").value
     sum.textContent = +priceBakalavr.textContent * +e.target.parentElement.querySelector("input").value;
     buySumValue.textContent = sum.textContent
     sumAll.textContent = +buySumValue.textContent + +rentSumValue.textContent + +logoValue.textContent 
   } else if (e.target.classList.contains("minus") && +e.target.parentElement.querySelector("input").value > 0) {
     --e.target.parentElement.querySelector("input").value;
+    buySizeCount.textContent = e.target.parentElement.querySelector("input").value
     inputSumValue.value = e.target.parentElement.querySelector("input").value
     sum.textContent = +priceBakalavr.textContent * +e.target.parentElement.querySelector("input").value;
     buySumValue.textContent = sum.textContent 
@@ -263,7 +288,7 @@ function handleCountRent (e) {
       priceBakalavrRent.textContent = '400'
     } 
     inputRentSumValue.value = +rentValue.value + +rentCapValue.value
-
+    rentSizeCount.textContent = inputRentSumValue.value
     sumRent.textContent = +priceBakalavrRent.textContent * +e.target.parentElement.querySelector("input").value;
     rentSumValue.textContent = +sumRentCap.textContent + +sumRent.textContent 
     sumAll.textContent = +buySumValue.textContent + +rentSumValue.textContent + +logoValue.textContent 
@@ -273,7 +298,7 @@ function handleCountRent (e) {
       priceBakalavrRent.textContent = '500'
     }  
     inputRentSumValue.value = +rentValue.value + +rentCapValue.value
-
+    rentSizeCount.textContent = inputRentSumValue.value
     sumRent.textContent = +priceBakalavrRent.textContent * +e.target.parentElement.querySelector("input").value;
     rentSumValue.textContent = +sumRentCap.textContent + +sumRent.textContent 
     sumAll.textContent = +buySumValue.textContent + +rentSumValue.textContent + +logoValue.textContent 
@@ -284,15 +309,27 @@ function handleCountRentCap (e) {
   if (e.target.classList.contains("plus-cap")) {
     ++e.target.parentElement.querySelector("input").value; 
     inputRentSumValue.value = +rentValue.value + +rentCapValue.value
+    rentSizeCount.textContent = inputRentSumValue.value
     sumRentCap.textContent = +priceBakalavrRentCap.textContent * +e.target.parentElement.querySelector("input").value;
     rentSumValue.textContent = +sumRentCap.textContent + +sumRent.textContent 
     sumAll.textContent = +buySumValue.textContent + +rentSumValue.textContent + +logoValue.textContent 
   } else if (e.target.classList.contains("minus-cap") && +e.target.parentElement.querySelector("input").value > 0) {
     --e.target.parentElement.querySelector("input").value;
     inputRentSumValue.value = +rentValue.value + +rentCapValue.value
+    rentSizeCount.textContent = inputRentSumValue.value
     sumRentCap.textContent = +priceBakalavrRentCap.textContent * +e.target.parentElement.querySelector("input").value;
     rentSumValue.textContent = +sumRentCap.textContent + +sumRent.textContent 
     sumAll.textContent = +buySumValue.textContent + +rentSumValue.textContent + +logoValue.textContent 
+  }
+}
+
+function handleToggleBrush () {
+  if (+rentValue.value > 0 && (+rentCapValue.value == 0 && +inputSumValue.value == 0) ) {
+    stepBrushShow.setAttribute('disabled', 'disabled')
+  } else if (+rentValue.value == 0 && (+rentCapValue.value == 0 && +inputSumValue.value == 0) ) {
+    stepBrushShow.setAttribute('disabled', 'disabled')
+  } else {
+    stepBrushShow.removeAttribute('disabled')
   }
 }
 
@@ -316,7 +353,49 @@ function handleCountLogoFancy (e) {
   }
 } 
 
+function validate() {
+  const remember = document.getElementById("address");
+  if (remember.checked) {
+    document.querySelector('.return').setAttribute('style', 'display:flex')
+  } else {
+    document.querySelector('.return').setAttribute('style', 'display:none')
+  }
+}
+validate()
+
+function showRent() {
+  if (+document.querySelector('.in-num__rent').value > 0 || +document.querySelector('.in-num__cap').value > 0) {
+    document.querySelector('.add-address').setAttribute('style', 'display:flex')
+    document.querySelector('.rent-size').setAttribute('style', 'display:block')
+  } else {
+    document.querySelector('.add-address').setAttribute('style', 'display:none')
+    document.querySelector('.rent-size').setAttribute('style', 'display:none')
+  }
+
+  if (+document.querySelector('.in-num__count').value > 0) {
+    document.querySelector('.buy-size').setAttribute('style', 'display:block')
+  } else {
+    document.querySelector('.buy-size').setAttribute('style', 'display:none')
+  }
+}
+
+
+// const checkbox = document.getElementById("politics");
+// const btn_submit = document.querySelector(".form__submit");
+
+// checkbox.addEventListener("change", () => {
+//     if (checkbox.checked) {
+//         btn_submit.removeAttribute("disabled");
+//     } else {
+//         btn_submit.setAttribute("disabled", true);
+//     }
+// });
+
+colorTabs.addEventListener('click', handleClickActiveColor)
+colorTabsMg.addEventListener('click', handleClickActiveColor)
+colorTieTabs.addEventListener('click', handleClickActiveColor)
 header.addEventListener('click', handleClickToggleActive)
+bodyStep1.addEventListener('click', handleClickActive)
 header.addEventListener('click', handleClickShowColor)
 formElement.addEventListener('submit', handleSubmitForm)
 formElement.addEventListener('click', handleCountBuy)
@@ -325,9 +404,10 @@ formElement.addEventListener('click', handleCountRentCap)
 formElement.addEventListener('click', handleCountLogoFancy)
 formElement.addEventListener('click', handleCountLogo)
 minus.addEventListener('click', handleSubmitForm)
-bodyStep1.addEventListener('click', handleClickActive)
+
+formElement.addEventListener('click', showRent)
+formElement.addEventListener('click', handleToggleBrush)
+
 // bodyStep1.addEventListener('click', handleChangeSrcMagistr)
-colorTabs.addEventListener('click', handleClickActiveColor)
-colorTabsMg.addEventListener('click', handleClickActiveColor)
-colorTieTabs.addEventListener('click', handleClickActiveColor)
+
 
